7673660559d4eff87a7551ab8ced7d3b6a6b5e5f
Jamen <31097748+Jamen1147@users.noreply.github.com>
2018-08-29 08:00:19 +1000

    Implement Models
    Implement Models for Student, Staff and Application

--- UTSLoanApp/app/src/main/java/com/sep/utsloanapp/models/Application.java
+++ UTSLoanApp/app/src/main/java/com/sep/utsloanapp/models/Application.java
@@ -1,4 +1,147 @@
 package com.sep.utsloanapp.models;
 
+/**
+ * Model class that forms the Application Object.
+ * Descriptions for unclear attributes below.
+ * -----------------------------------------
+ * LoanPeriodYear - 1 Year to 4 Year
+ * RepaymentPeriodMonth - 6 month to 60 month
+ * Result - 0 rejected, 1 approved
+ * Status - "Submitted", "Processing", "Approved or Rejected"
+ * -----------------------------------------
+ * */
 public class Application {
+
+    private String mApplicationId, mTimeSubmitted, mTimeDeclared, mStatus, mAmount, mUsage,
+            mOtherUsage, mRejectReason;
+
+    private int mStudentId, mStaffId, mLoanPeriodYear, mRepaymentPeriodMonth, mResult;
+
+    public Application() {
+    }
+
+    public Application(String applicationId, String timeSubmitted, String timeDeclared,
+                       String status, String amount, String usage, String otherUsage,
+                       String rejectReason, int studentId, int staffId, int loanPeriodYear,
+                       int repaymentPeriodMonth, int result) {
+
+        mApplicationId = applicationId;
+        mTimeSubmitted = timeSubmitted;
+        mTimeDeclared = timeDeclared;
+        mStatus = status;
+        mAmount = amount;
+        mUsage = usage;
+        mOtherUsage = otherUsage;
+        mRejectReason = rejectReason;
+        mStudentId = studentId;
+        mStaffId = staffId;
+        mLoanPeriodYear = loanPeriodYear;
+        mRepaymentPeriodMonth = repaymentPeriodMonth;
+        mResult = result;
+
+    }
+
+    public String getApplicationId() {
+        return mApplicationId;
+    }
+
+    public void setApplicationId(String applicationId) {
+        mApplicationId = applicationId;
+    }
+
+    public String getTimeSubmitted() {
+        return mTimeSubmitted;
+    }
+
+    public void setTimeSubmitted(String timeSubmitted) {
+        mTimeSubmitted = timeSubmitted;
+    }
+
+    public String getTimeDeclared() {
+        return mTimeDeclared;
+    }
+
+    public void setTimeDeclared(String timeDeclared) {
+        mTimeDeclared = timeDeclared;
+    }
+
+    public String getStatus() {
+        return mStatus;
+    }
+
+    public void setStatus(String status) {
+        mStatus = status;
+    }
+
+    public String getAmount() {
+        return mAmount;
+    }
+
+    public void setAmount(String amount) {
+        mAmount = amount;
+    }
+
+    public String getUsage() {
+        return mUsage;
+    }
+
+    public void setUsage(String usage) {
+        mUsage = usage;
+    }
+
+    public String getOtherUsage() {
+        return mOtherUsage;
+    }
+
+    public void setOtherUsage(String otherUsage) {
+        mOtherUsage = otherUsage;
+    }
+
+    public String getRejectReason() {
+        return mRejectReason;
+    }
+
+    public void setRejectReason(String rejectReason) {
+        mRejectReason = rejectReason;
+    }
+
+    public int getStudentId() {
+        return mStudentId;
+    }
+
+    public void setStudentId(int studentId) {
+        mStudentId = studentId;
+    }
+
+    public int getStaffId() {
+        return mStaffId;
+    }
+
+    public void setStaffId(int staffId) {
+        mStaffId = staffId;
+    }
+
+    public int getLoanPeriodYear() {
+        return mLoanPeriodYear;
+    }
+
+    public void setLoanPeriodYear(int loanPeriodYear) {
+        mLoanPeriodYear = loanPeriodYear;
+    }
+
+    public int getRepaymentPeriodMonth() {
+        return mRepaymentPeriodMonth;
+    }
+
+    public void setRepaymentPeriodMonth(int repaymentPeriodMonth) {
+        mRepaymentPeriodMonth = repaymentPeriodMonth;
+    }
+
+    public int getResult() {
+        return mResult;
+    }
+
+    public void setResult(int result) {
+        mResult = result;
+    }
 }
--- UTSLoanApp/app/src/main/java/com/sep/utsloanapp/models/Staff.java
+++ UTSLoanApp/app/src/main/java/com/sep/utsloanapp/models/Staff.java
@@ -1,4 +1,113 @@
 package com.sep.utsloanapp.models;
 
+/**
+ * Model class that forms the Staff Object.
+ * Descriptions for unclear attributes below.
+ * -----------------------------------------
+ * Gender - 0 male, 1 female, 2 other
+ * -----------------------------------------
+ * */
 public class Staff {
+
+    private String mUid, mFirstName, mLastName, mEmail, mPhoneNumber, mDOB;
+    private String mDateEmployed;
+    private int mStaffId, mGender, mTimesApplicationDeclared;
+
+    public Staff() {
+    }
+
+    public Staff(String uid, String firstName, String lastName, String email, String phoneNumber,
+                 String DOB, String dateEmployed, int staffId, int gender,
+                 int timesApplicationDeclared) {
+        mUid = uid;
+        mFirstName = firstName;
+        mLastName = lastName;
+        mEmail = email;
+        mPhoneNumber = phoneNumber;
+        mDOB = DOB;
+        mDateEmployed = dateEmployed;
+        mStaffId = staffId;
+        mGender = gender;
+        mTimesApplicationDeclared = timesApplicationDeclared;
+    }
+
+    public String getUid() {
+        return mUid;
+    }
+
+    public void setUid(String uid) {
+        mUid = uid;
+    }
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        mFirstName = firstName;
+    }
+
+    public String getLastName() {
+        return mLastName;
+    }
+
+    public void setLastName(String lastName) {
+        mLastName = lastName;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public void setEmail(String email) {
+        mEmail = email;
+    }
+
+    public String getPhoneNumber() {
+        return mPhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        mPhoneNumber = phoneNumber;
+    }
+
+    public String getDOB() {
+        return mDOB;
+    }
+
+    public void setDOB(String DOB) {
+        mDOB = DOB;
+    }
+
+    public String getDateEmployed() {
+        return mDateEmployed;
+    }
+
+    public void setDateEmployed(String dateEmployed) {
+        mDateEmployed = dateEmployed;
+    }
+
+    public int getStaffId() {
+        return mStaffId;
+    }
+
+    public void setStaffId(int staffId) {
+        mStaffId = staffId;
+    }
+
+    public int getGender() {
+        return mGender;
+    }
+
+    public void setGender(int gender) {
+        mGender = gender;
+    }
+
+    public int getTimesApplicationDeclared() {
+        return mTimesApplicationDeclared;
+    }
+
+    public void setTimesApplicationDeclared(int timesApplicationDeclared) {
+        mTimesApplicationDeclared = timesApplicationDeclared;
+    }
 }
